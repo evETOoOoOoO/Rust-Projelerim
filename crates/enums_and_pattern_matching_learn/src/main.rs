7f6aebca129bba93cb4694c3cd806b924f16101e
Ahmet Asaf Güleç <ahmetasafgulec@gmail.com>
2026-07-07 14:56:01 +0300

    Update

--- crates/enums_and_pattern_matching_learn/src/main.rs
+++ crates/enums_and_pattern_matching_learn/src/main.rs
@@ -1,8 +1,35 @@
+use std::io;
+
 enum Direction {
-    Up
-    Down
-    Right
-    Left
+    Up,
+    Down,
+    Right,
+    Left,
 }
 
-fn main() {}
+fn main() {
+    println!("Please say a enum (Up / Down / Right / Left): ");
+    let mut answer = String::new();
+    io::stdin()
+        .read_line(&mut answer)
+        .expect("Failed to read line");
+    let answer = answer.trim().to_uppercase();
+
+    let direction = match answer.as_str() {
+        "UP" => Direction::Up,
+        "DOWN" => Direction::Down,
+        "RIGHT" => Direction::Right,
+        "LEFT" => Direction::Left,
+        _ => {
+            println!("Invalid choice");
+            return;
+        }
+    };
+
+    match direction {
+        Direction::Up => println!("Moving up"),
+        Direction::Down => println!("Moving down"),
+        Direction::Right => println!("Moving right"),
+        Direction::Left => println!("Moving left"),
+    }
+}
