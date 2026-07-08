8630ad5dedc9daba865b8c9fe5e760504d70fa80
Ahmet Asaf Güleç <ahmetasafgulec@gmail.com>
2026-07-08 11:32:55 +0300

    Todo list

---
+++ crates/todo_list/src/main.rs
@@ -0,0 +1,90 @@
+use std::io;
+
+struct Todo {
+    id: u32,
+    title: String,
+    completed: bool,
+}
+
+fn main() {
+    let mut tasks: Vec<Todo> = Vec::new();
+
+    loop {
+        println!("\n=== Todo List ===");
+        println!("1. Görev ekle");
+        println!("2. Görevleri listele");
+        println!("3. Görevi tamamlandı işaretle");
+        println!("4. Çıkış");
+        println!("Seçiminiz:");
+
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
+        let choice = choice.trim();
+
+        match choice {
+            "1" => {
+                println!("Görev başlığını girin:");
+                let mut title = String::new();
+                io::stdin()
+                    .read_line(&mut title)
+                    .expect("Failed to read line");
+                let title = title.trim().to_string();
+
+                let new_task = Todo {
+                    id: tasks.len() as u32 + 1,
+                    title: title,
+                    completed: false,
+                };
+
+                tasks.push(new_task);
+                println!("Görev eklendi!");
+            }
+            "2" => {
+                if tasks.is_empty() {
+                    println!("Henüz görev yok");
+                } else {
+                    for task in &tasks {
+                        let durum = if task.completed { "✓" } else { " " };
+                        println!("[{}] {} - {}", durum, task.id, task.title);
+                    }
+                }
+            }
+            "3" => {
+                println!("Hangi görevin id'sini tamamlandı işaretlemek istiyorsunuz?");
+                let mut id_input = String::new();
+                io::stdin()
+                    .read_line(&mut id_input)
+                    .expect("Failed to read line");
+                let id_input: u32 = match id_input.trim().parse() {
+                    Ok(id) => id,
+                    Err(_) => {
+                        println!("Geçersiz sayı. ");
+                        continue;
+                    }
+                };
+
+                let mut found = false;
+
+                for task in &mut tasks {
+                    if task.id == id_input {
+                        task.completed = true;
+                        found = true;
+                        println!("Görev tamamlandı olarak işaretlendi!");
+                    }
+                }
+                if !found {
+                    println!("Görev Bulunamadı");
+                }
+            }
+            "4" => {
+                println!("Görüşürüz!");
+                break;
+            }
+            _ => {
+                println!("Geçersiz seçim, tekrar deneyin.");
+            }
+        }
+    }
+}
