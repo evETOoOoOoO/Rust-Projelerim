8804b931c1024f9623e2d0bea8119bd40eece6e5
Ahmet Asaf Güleç <ahmetasafgulec@gmail.com>
2026-08-25 19:08:46 +0300

    update

--- crates/deneme/src/main.rs
+++ crates/deneme/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+/*
 fn main() {
     println!("deneme");
+}
+*/
+
+fn main() {
+    for i in 1..101 {
+        println!("{i}");
+
+        if (i == 50) {
+            println!("yolun yarısı");
+        }
+    }
 }
